2d59c8c0492031427e8547a842e7b85dafee4f42
youness  gourti <younessgourti74@gmail.com>
2025-08-05 20:06:36 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './App.css';
+
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About  from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Scrollup from './components/scrollup/Scrollup';
+
+
+
+const App = () => {
+  return (
+    <>
+     <Header />
+
+     <main className='main'>
+      <Home />
+      <About />
+      <Skills />
+      <Services />
+      <Qualification />
+      <Testimonials />
+      <Contact />
+      
+
+     </main>
+     <Footer />
+     <Scrollup />
+    </>
+  );
+}
+
+export default App;
